70210b7fe9e7ae951c59249e5bf74db2da7e920c
Shane <shaneiadt@gmail.com>
2016-04-13 16:07:35 +0100

    Added files via upload

---
+++ find_the_longest_word.js
@@ -0,0 +1,9 @@
+function findLongestWord(str) {
+    var strArr = str.split(' ');
+    var longestStr = '';
+    for(var i=0;i<strArr.length;i++){
+        longestStr = (strArr[i].length > longestStr.length)?strArr[i]:longestStr;
+    }
+    return longestStr.length;
+}
+findLongestWord("The quick brown fox jumped over the lazy dog");
