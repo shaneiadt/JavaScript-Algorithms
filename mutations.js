cd6aeec9bdcd2c0aaca0b8ac1bf846ba661cda8d
Shane <shaneiadt@gmail.com>
2016-04-14 15:08:40 +0100

    Added files via upload

---
+++ mutations.js
@@ -0,0 +1,11 @@
+function mutation(arr) {
+    var arr1 = arr[0].toLowerCase().split(''), arr2 = arr[1].toLowerCase().split(''), truth = 0;
+    for(var i=0;i<arr1.length;i++){
+        for(var j=0;j<arr2.length;j++){
+            if(arr1[i].indexOf(arr2[j])!=-1){truth++;}
+        }
+    }
+    return (truth >= arr2.length)?true:false;
+}
+
+mutation(["hello", "hey"]);
