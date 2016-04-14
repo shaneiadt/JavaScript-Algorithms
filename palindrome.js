f199bc565fdd54bf588581b24513b35002ef7fd7
Shane <shaneiadt@gmail.com>
2016-04-14 15:20:06 +0100

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,3 +1,10 @@
+/*
+Is the provided String a 'palindrome' after removing all non-aplhanumeric characters & spaces? If 'yes' return true, if 'no' return false.
+
+NOTE: A palindrome is a word that is the same forward & backward.
+
+*/
+
 function palindrome(str) {
   var tempStr = str;
   return (str.replace(/[\W_]+/g, '').split('').reverse().join('').toLowerCase()===tempStr.replace(/[\W_]+/g,'').toLowerCase())?true:false;
