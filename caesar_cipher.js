6f0fefc064c7c813a6a64944c778f788f0eb704c
Shane <shaneiadt@gmail.com>
2016-04-14 18:19:13 +0100

    Added files via upload

---
+++ caesar_cipher.js
@@ -0,0 +1,15 @@
+/*
+Casesar Cipher ROT13 https://en.wikipedia.org/wiki/ROT13
+*/
+
+function rot13(str) {
+    var betUnscrambled = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''), betScrambled = 'NOPQRSTUVWXYZABCDEFGHIJKLM'.split('');
+    str = str.split('');
+    var newStr = "";
+    for(var i=0;i<str.length;i++){
+        newStr += (betScrambled.indexOf(str[i])!==-1)?betUnscrambled[betScrambled.indexOf(str[i])]:str[i];
+    }
+    return newStr;
+}
+
+rot13("GUR DHVPX OEBJA QBT WHZCRQ BIRE GUR YNML SBK.");
