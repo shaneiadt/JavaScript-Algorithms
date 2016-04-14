58f1dacd8c8a6eb51b76219e95636434ef7d2a02
Shane <shaneiadt@gmail.com>
2016-04-14 15:13:40 +0100

    Update chunky_monkey.js

--- chunky_monkey.js
+++ chunky_monkey.js
@@ -1,3 +1,7 @@
+/*
+Split the provided array into smaller arrays of length 'size' & return a two-dimensional array containing these chunks.
+*/
+
 function chunk(arr, size) {  
   var tempArr = [], i = 0;
   while (i < arr.length) {tempArr.push(arr.slice(i, i += size));}
