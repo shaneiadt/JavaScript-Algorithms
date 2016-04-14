b28e7bf75de15c16796ade024c38a5525ab4a470
Shane <shaneiadt@gmail.com>
2016-04-14 15:14:45 +0100

    Update capatilize_each_word.js

--- capatilize_each_word.js
+++ capatilize_each_word.js
@@ -1,3 +1,7 @@
+/*
+Return a string where all words are capatilized.
+*/
+
 function titleCase(str) {
   return str.replace(/\w\S*/g, function(ts) { return ts.charAt(0).toUpperCase() + ts.substr(1).toLowerCase(); });
 }
