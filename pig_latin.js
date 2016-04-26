8b634aae266c773a64b1714fb073d1a3eed39677
Shane <shaneiadt@gmail.com>
2016-04-26 13:01:38 +0100

    Create pig_latin.js

---
+++ pig_latin.js
@@ -0,0 +1,25 @@
+/*
+Convert string into Pig Latin (https://en.wikipedia.org/wiki/Pig_Latin)
+*/
+function translatePigLatin(str) {
+    
+    var vowels = ['a','e','i','o','u'], newStr = '', strArr = str.split(''), tempCon = '';
+    
+    if(vowels.indexOf(strArr[0])>=0){
+        newStr = strArr.join('')+'way';
+    }else{
+        for(var i=0;i<strArr.length;i++){
+            if(vowels.indexOf(strArr[i])===-1){
+                tempCon += strArr[i];
+                delete strArr[i];
+            }else{
+                newStr = strArr.join('')+tempCon+'ay';
+                break;
+            }
+        }
+    }
+    
+    return newStr;
+}
+
+translatePigLatin("glove");
