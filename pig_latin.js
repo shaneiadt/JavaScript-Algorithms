02bf6267d657849ede219cc65839ff0d573e89d1
Shane <shaneiadt@gmail.com>
2016-04-26 13:01:43 +0100

    Added files via upload

--- pig_latin.js
+++ pig_latin.js
@@ -3,11 +3,12 @@ Convert string into Pig Latin (https://en.wikipedia.org/wiki/Pig_Latin)
 */
 function translatePigLatin(str) {
     
-    var vowels = ['a','e','i','o','u'], newStr = '', strArr = str.split(''), tempCon = '';
+    var vowels = ['a','e','i','o','u'], newStr = '', strArr = str.split('');
     
     if(vowels.indexOf(strArr[0])>=0){
         newStr = strArr.join('')+'way';
     }else{
+        var tempCon = '';
         for(var i=0;i<strArr.length;i++){
             if(vowels.indexOf(strArr[i])===-1){
                 tempCon += strArr[i];
